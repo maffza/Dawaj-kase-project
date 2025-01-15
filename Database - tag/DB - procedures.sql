42b9e0fe74f3d16c088b3fc9cae10b1575c0d3f3
jkozienski <kuba.kozienski2203@gmail.com>
2025-01-15 20:24:24 +0100

    Revert "Moved "get_comments_by_project_id" into database function"
    This reverts commit 78edda282232b0a7425ad1276ebf05977c16ea54.

--- Database - tag/DB - procedures.sql
+++ Database - tag/DB - procedures.sql
@@ -1,59 +1,8 @@
-CREATE OR REPLACE PACKAGE Crowdfunding_pkg AS
-    FUNCTION get_comments_by_campaign_id(
-        p_campaign_id IN NUMBER
-    ) RETURN SYS_REFCURSOR;
-     FUNCTION get_campaign_by_id(
-        p_id IN NUMBER
-    ) RETURN SYS_REFCURSOR;
-END Crowdfunding_pkg;
-/
-
-CREATE OR REPLACE PACKAGE BODY Crowdfunding_pkg AS
-
-    FUNCTION get_comments_by_campaign_id(
-        p_campaign_id IN NUMBER
-    ) RETURN SYS_REFCURSOR
-    IS
-        result_cursor SYS_REFCURSOR;
-    BEGIN
-        -- Open a SYS_REFCURSOR and assign the result of the SELECT query to it
-        OPEN result_cursor FOR
-            SELECT 
-                c.id AS comment_id,
-                c.comment_text,
-                c.creation_date,
-                c.modify_date,
-                u.first_name || ' ' || u.last_name AS username
-            FROM 
-                comments c
-            JOIN 
-                users u 
-            ON 
-                c.user_id = u.id
-            WHERE 
-                c.campaign_id = p_campaign_id
-            ORDER BY 
-                c.id DESC;
-
-        -- Return the cursor
-        RETURN result_cursor;
-    END get_comments_by_campaign_id;
-
-    FUNCTION get_campaign_by_id(
-        p_id IN NUMBER
-    ) RETURN SYS_REFCURSOR 
-    IS
-        ref_cursor SYS_REFCURSOR;
-    BEGIN
-      OPEN ref_cursor FOR
-        SELECT 
-            * 
-        FROM 
-            campaigns 
-        WHERE 
-            id = p_id;
-      RETURN ref_cursor;
-    END get_campaign_by_id;
-
-END Crowdfunding_pkg;
-/
+CREATE OR REPLACE FUNCTION get_campaign_by_id(p_id IN NUMBER) 
+RETURN SYS_REFCURSOR AS
+  ref_cursor SYS_REFCURSOR;
+BEGIN
+  OPEN ref_cursor FOR
+    SELECT * FROM campaigns WHERE id = p_id;
+  RETURN ref_cursor;
+END;
--- DawajKase/main/Managers/CommentManager.py
+++ DawajKase/main/Managers/CommentManager.py
@@ -1,6 +1,6 @@
 from django.db import connection
 from ..Comment import Comment
-import oracledb
+import cx_Oracle
 
 class CommentManager:
     @staticmethod
@@ -8,8 +8,13 @@ class CommentManager:
         comments = None
         
         with connection.cursor() as cursor:
-            ref_cursor = cursor.callfunc("Crowdfunding_pkg.get_comments_by_campaign_id", oracledb.CURSOR, [int(campaignID)])
-            commentsResult = ref_cursor.fetchall()
+            cursor.execute("SELECT c.id, c.comment_text, c.creation_date, c.modify_date, u.first_name || ' ' || u.last_name AS username FROM comments c JOIN users u ON c.user_id=u.id WHERE c.campaign_id=%s ORDER BY c.id DESC;", [campaignID])
+            #result_cursor = cursor.var(cx_Oracle.CURSOR)
+            
+            #cursor.callproc("Crowdfunding_pkg.get_comments_by_campaign_id", [campaignID, result_cursor])
+            
+            #ref_cursor = result_cursor.getvalue()
+            commentsResult = cursor.fetchall()
             
             if commentsResult:
                 comments = [Comment(*c).to_json() for c in commentsResult]
